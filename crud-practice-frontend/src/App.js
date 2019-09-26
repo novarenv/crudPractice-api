10419cd7171af6eaa35736d0f287bac5c38a0317
Novaren Veraldo <novarenv@gmail.com>
2019-09-27 00:58:16 +0700

    CRUD API done

--- crud-practice-frontend/src/App.js
+++ crud-practice-frontend/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   }
 
   getItems(){
-    fetch('http://localhost:3000/')
+    fetch('http://localhost:3000/crud')
       .then(response => response.json())
       .then(items => this.setState({items}))
       .catch(err => console.log(err))
